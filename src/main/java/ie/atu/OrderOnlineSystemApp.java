934104009d3caf2a5401dc6f14b6d4eaba09662f
Mark Carroll <G00419267@atu.ie>
2024-02-08 12:30:55 +0000

    Dessert added

--- src/main/java/ie/atu/OrderOnlineSystemApp.java
+++ src/main/java/ie/atu/OrderOnlineSystemApp.java
@@ -7,10 +7,20 @@ public class OrderOnlineSystemApp {
 
                 new Burger("Chee",19,"Chee"),
                 new Pizza("Chee Peez",12,"Chee & Peez"),
-                new Salad("Chee Peez",19,"Chee & Peez"),
-
-
+                new Salad("Chee Seed",19,"Chee & Seed"),
         };
+
+        for(MenuItem menuItem : menuItems){
+            System.out.println("Name: " +menuItem.getName() );
+            System.out.println("Price: " +menuItem.getPrice() );
+            System.out.println("Ingrediants: " +menuItem.getDesc() + "\n");
+        }
+
+        double totalCost = 0;
+        for(MenuItem menuItem : menuItems){
+            totalCost += menuItem.getPrice();
+        }
+        System.out.println("Total cost: €"+totalCost);
 
     }
 }
