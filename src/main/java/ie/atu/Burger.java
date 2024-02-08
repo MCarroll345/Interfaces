63b03f8787d3dad44603f5f1a9bae96514038042
Mark Carroll <G00419267@atu.ie>
2024-02-08 12:14:22 +0000

    Interface

--- src/main/java/ie/atu/Burger.java
+++ src/main/java/ie/atu/Burger.java
@@ -1,4 +1,29 @@
 package ie.atu;
 
-public class Burger {
+public class Burger implements MenuItem {
+    private double price;
+    private String name;
+    private String ingrediants;
+
+    public Burger(String name, double price, String ingrediants){
+        this.name = name;
+        this.price = price;
+        this.ingrediants = ingrediants;
+    }
+
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String getDesc() {
+        return ingrediants;
+    }
 }
--- src/main/java/ie/atu/MenuItem.java
+++ src/main/java/ie/atu/MenuItem.java
@@ -7,6 +7,4 @@ public interface MenuItem {
     String getDesc();
 
 
-
-
 }
